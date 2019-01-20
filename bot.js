1bc0b31b61e701bb94b181429cf652ea21e7279f
sourov <38147368+sourovafrin@users.noreply.github.com>
2019-01-21 03:21:33 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const { Client, Attachment } = require('discord.js');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '.';
-var am =;
+var am = [] ;
 
 
 client.on('ready', () => {
