a324bf0edcbfd3dff653f0591df930562ee344f2
sourov <38147368+sourovafrin@users.noreply.github.com>
2019-01-21 04:41:36 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,36 +27,9 @@ client.on('message', message => {
       const attachment = new Attachment('http://gif-finder.com/wp-content/uploads/2017/05/Gillian-Jacobs-Fuck-You.gif');
       message.channel.send(attachment);}
   
-   if (message.content.startsWith(prefix + 'clr')) { 
-        async function purge() 
-
-            // Now, we want to check if the user has the `bot-commander` role, you can change this to whatever you want.
-            if (!message.member.roles.find("name", "Owner")) { // This checks to see if they DONT have it, the "!" inverts the true/false
-                message.channel.send('You need the \`Owner\` role to use this command.'); // This tells the user in chat that they need the role.
-                return; // this returns the code, so the rest doesn't run.
-            }
-
-            // We want to check if the argument is a number
-            if (isNaN(args[0])) {
-                // Sends a message to the channel.
-                message.channel.send('Please use a number as your arguments. \n Usage: ' + prefix + 'clr <amount>'); //\n means new line.
-                // Cancels out of the script, so the rest doesn't run.
-                return;
-            }
-
-            const fetched = await message.channel.fetchMessages({limit: args[0]}); // This grabs the last number(args) of messages in the channel.
-            console.log(fetched.size + ' messages found, deleting...'); // Lets post into console how many messages we are deleting
-
-            // Deleting the messages
-            message.channel.bulkDelete(fetched)
-                .catch(error => message.channel.send(`Error: ${error}`)); // If it finds an error, it posts it into the channel.
-
-        }
-
-        // We want to make sure we call the function whenever the purge command is run.
-        purge(); // Make sure this is inside the if(msg.startsWith)
-
-    }
+   if (message.content.startsWith(prefix + 'clr') { 
+       const fetched = await message.channel.fetchMessages({limit: args[0]});
+        message.channel.bulkDelete(fetched)}
     
 });
 
