6c7bc035ae8f4100142afff2f0fd4eb21b15e9c6
sourov <38147368+sourovafrin@users.noreply.github.com>
2019-01-21 05:06:04 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,16 +3,9 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '.';
 
-
-
-
-bot.on('message', message => {
-
-    // Variables - Variables make it easy to call things, since it requires less typing.
-    let msg = message.content.toUpperCase(); // This variable takes the message, and turns it all into uppercase so it isn't case sensitive.
-    let sender = message.author; // This variable takes the message, and finds who the author is.
-    let cont = message.content.slice(prefix.length).split(" "); // This variable slices off the prefix, then puts the rest in an array based off the spaces
-    let args = cont.slice(1);
+client.on('ready', () => {
+  console.log('I am ready!');
+});
 
 client.on('message', message => {
   if (msg === prefix + 'hi') {
@@ -31,16 +24,8 @@ client.on('message', message => {
   if (msg === prefix + 'fck') {
       const attachment = new Attachment('http://gif-finder.com/wp-content/uploads/2017/05/Gillian-Jacobs-Fuck-You.gif');
       message.channel.send(attachment);}
-  
-   if (msg.startsWith(prefix + 'clr')) {
-       async function purge(){
-       const fetched = await message.channel.fetchMessages({limit: args[0]});
-        message.channel.bulkDelete(fetched)}
-  purge();}
           
 });
-client.on('ready', () => {
-  console.log('I am ready!');
-});
+
 // Log our bot in using the token from https://discordapp.com/developers/applications/me
 client.login(process.env.BOT_TOKEN);
