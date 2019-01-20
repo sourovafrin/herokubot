d864000b4b06b15623d2a621c4f0fa65e0b4fd1a
sourov <38147368+sourovafrin@users.noreply.github.com>
2019-01-21 01:23:24 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
-const Discord = require('discord.js');
 // Extract the required classes from the discord.js module
-const { Client, Attachment } = require('discord.js');
+const { Client, MessageAttachment } = require('discord.js');
 
 // Create an instance of a Discord client
 const client = new Client();
@@ -14,12 +13,12 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    // If the message is '!rip'
-    if (message.content === '!rip') {
-        // Create the attachment using Attachment
-        const attachment = new Attachment('https://i.imgur.com/w3duR07.png');
-        // Send the attachment in the message channel
-        message.channel.send(attachment);
-    }
+	// If the message is '!rip'
+	if (message.content === '!rip') {
+		// Create the attachment using MessageAttachment
+		const attachment = new MessageAttachment('https://i.imgur.com/w3duR07.png');
+		// Send the attachment in the message channel
+		message.channel.send(attachment);
+	}
 });
 client.login(process.env.BOT_TOKEN);
