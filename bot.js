448abd25918d2a1e13b2042727c18cfbb8989849
sourov <38147368+sourovafrin@users.noreply.github.com>
2019-01-21 00:59:32 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,9 +11,9 @@ client.on('ready', () => {
 client.on('message', message => {
   if (message.content === prefix + 'hi') {
      message.channel.send('hello'); 
- }if (message.content === prefix + 'ping') {
+  if (message.content === prefix + 'ping') {
      message.channel.send(new Date().getTime() - message.createdTimestamp + " ms");        
- }if (message.content === prefx + 'avtr') {
+ if (message.content === prefx + 'avtr') {
      message.reply(message.author.avatarURL);
-};
+});
 client.login(process.env.BOT_TOKEN);
