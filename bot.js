e0c28621f67a0e5c37df1ee79029512f516cfeca
sourov <38147368+sourovafrin@users.noreply.github.com>
2019-01-21 01:11:30 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ client.on('message', message => {
     // If the message is '!rip'
     if (message.content === '!rip') {
         // Create the attachment using Attachment
-        const attachment = new Attachment('https://i.imgur.com/w3duR07.png');
+        const attachment = Attachment('https://i.imgur.com/w3duR07.png');
         // Send the attachment in the message channel
         message.channel.send(attachment);
     }
