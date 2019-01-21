5eddcb478ce26e0e8db202d939a62a4221d6403a
sourov <38147368+sourovafrin@users.noreply.github.com>
2019-01-21 17:58:23 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,52 +2,6 @@ const { Client, Attachment } = require('discord.js');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '.';
-const { Command } = require('discord.js-commando');
-
-module.exports= class ClearCommand extends Command {
-  constructor(client) {
-    super(client, {
-      name: 'clear',
-      group: 'action',
-      memberName: 'clear',
-      description: 'Clear messages',
-      examples: ['clear 10'],
-      clientPermissions: ['MANAGE_MESSAGES'],
-      userPermissions: ['MANAGE_MESSAGES'],
-      guildOnly: true,
-      args: [
-        {
-          key: 'howMany',
-          prompt: 'Please enter steemit.com post URL.',
-          type: 'integer',
-          default: 50,
-        },
-      ],
-      argsPromptLimit: 0,
-    });
-  }
-
-  hasPermission(message) {
-    if (!message.member.roles.some(role => ['Admin'].includes(role.name))) {
-      return 'you do not have required permission to clear messages!';
-    }
-    return true;
-  }
-
-  async run(message, { howMany }) {
-    message.delete();
-    const fetched = await message.channel.fetchMessages({ limit: howMany });
-    try {
-      message.channel.bulkDelete(fetched);
-    } catch (e) {
-      console.error(e.stack);
-    }
-  }
-}
-
-client.on('ready', () => {
-  console.log('I am ready!');
-});
 
 client.on('message', message => {
   if (message.content === prefix + 'hi') {
